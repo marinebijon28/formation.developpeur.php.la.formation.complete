fd24ea5a0ad17f1c061e76b5d970c8f87fd34187
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2022-02-11 10:15:29 +0100

    section 1 first step
    section 1 first step

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Premier pas en php
+        </title>
+    </head>
+    <body>
+        <h1>
+            Premier pas dans la programmation
+        </h1>
+        <h2>
+            Suivez le guide
+        </h2>
+        <?php
+            echo 'Je m\'appelle Mohamed <br/>';
+            echo "Je m'appelle Mohamed <br/>";
+
+            // Since PHP 8 you can use quotes within quotes
+            echo "Cette formation va abordé le ”PHP” tout au long de ces vidéos<br/>";
+            // PHP version 7
+            echo "Cette formation va abordé le \”PHP\” tout au long de ces vidéos<br/>";
+            echo 'Cette formation va abordé le ”PHP” tout au long de ces vidéos<br/>';
+
+            // string
+            echo "45<br/>";
+            // int
+            echo 65;
+
+            print "<br/>Mon nom est Mohamed";
+            print '<br/>Mon nom est Mohamed';
+        ?>
+    </body>
+</html>
